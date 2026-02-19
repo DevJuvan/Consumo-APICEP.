38cc456192e38cc93ed7263f1c835c32bb5ade88
DavJuvan <juvanildosilva961@gmail.com>
2026-02-19 01:42:18 -0300

    Adcionei o localStorage para persistir o cep informado

--- script.js
+++ script.js
@@ -12,16 +12,22 @@ let input = document.getElementById("cep").addEventListener("blur", () => {
                 if (!dados.error) {
                     console.log(dados)
 
-                        document.getElementById("rua").value = dados.logradouro;
-                        document.getElementById("bairro").value = dados.bairro;
-                        document.getElementById("cidade").value = dados.localidade;
-                        document.getElementById("estado").value = dados.uf;
-                          document.getElementById("numero").value = dados.ddd
-               
-                
+                    document.getElementById("rua").value = dados.logradouro;
+                    document.getElementById("bairro").value = dados.bairro;
+                    document.getElementById("cidade").value = dados.localidade;
+                    document.getElementById("estado").value = dados.uf;
+                    document.getElementById("numero").value = dados.ddd
 
-            }
+
+
+                }
             })
+
+
+
+
+
+
 
     }
     else {
@@ -29,6 +35,36 @@ let input = document.getElementById("cep").addEventListener("blur", () => {
     }
 
 
+
+});
+// aqui vou salvar os dados do input
+document.addEventListener('DOMContentLoaded', () => {
+
+  const inputs = document.querySelectorAll("input");
+
+  // SALVAR quando digitar
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      localStorage.setItem(input.id, input.value);
+    });
+  });
+
+  // RECUPERAR quando carregar
+  inputs.forEach((input) => {
+    const valorSalvo = localStorage.getItem(input.id);
+
+    if (valorSalvo) {
+      input.value = valorSalvo;
+    }
+  });
+
 });
 
 
+
+
+
+//setitem(key, value): metodo usado para salvar dados no localstorage
+//getitem(key): metodo usado para metodo usado para recuperar dados armazenados no localstoarge
+//removeitem(key): metodo usado para metodo usado para remover um item em especifico do localstorage
+//clear(): metodo usado para metodo usado para remover todos os itens armazenados no localStorage
